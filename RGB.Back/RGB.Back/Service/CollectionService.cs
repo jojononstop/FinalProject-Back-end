e9b9812f1cbb877d698e4c4472f3a04507e8d546
xftfchgg <k6663668@gmail.com>
2024-03-12 10:55:12 +0800

    m and c no complete service

---
+++ RGB.Back/RGB.Back/Service/CollectionService.cs
@@ -0,0 +1,39 @@
+﻿using RGB.Back.DTOs;
+using RGB.Back.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RGB.Back.Service
+{
+	public class CollectionService
+	{
+		private readonly RizzContext _context;
+		public CollectionService(RizzContext context)
+		{
+			_context = context;
+		}
+
+		public List<CollectionDTO> GetCollectionDetailByMemberId(int memberId)
+		{
+
+			var collectionList = _context.Collections.AsNoTracking()
+				.Where(c => c.MemberId == memberId)
+				.Include(c=>c.BillDetail)
+				.Select(c => new CollectionDTO
+				{
+					Id = c.Id,
+					GameId = c.GameId,
+					MemberTagId =c.MemberTagId,
+					DateOfPurchase = c.BillDetail.TransactionDate
+				})
+				.Distinct()
+				.ToList();
+
+			return collectionList;
+		}
+
+		public void EditCollection(int collectionId)
+		{
+
+		}
+	}
+}
---
+++ RGB.Back/RGB.Back/Service/MemberService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RGB.Back.DTOs;
+using RGB.Back.Models;
+
+namespace RGB.Back.Service
+{
+	public class MemberService
+	{
+		private readonly RizzContext _context;
+		public MemberService(RizzContext context)
+		{
+			_context = context;
+		}
+
+		public MemberDTO GetMemberDetailByMemberId(int memberId)
+		{
+			var memberDto = new MemberDTO();
+			var member = _context.Members.AsNoTracking()
+	                  .Where(x => x.Id == memberId)
+	                  .FirstOrDefault();
+			memberDto.Id = member.Id;
+			memberDto.Account = member.Account;
+			memberDto.Mail = member.Mail;
+			memberDto.AvatarUrl = member.AvatarUrl;
+			memberDto.RegistrationDate = member.RegistrationDate;
+			memberDto.BanTime = member.BanTime;
+			memberDto.Bonus = member.Bonus;
+			memberDto.LastLoginDate = member.LastLoginDate;
+			memberDto.Birthday = member.Birthday;
+			memberDto.NickName = member.NickName;
+
+			return memberDto;
+		}
+
+
+	}
+}
