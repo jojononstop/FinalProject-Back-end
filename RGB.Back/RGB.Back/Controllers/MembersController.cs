e530d61b0f075c5f756f2465d17b19a0b22a32c1
xftfchgg <k6663668@gmail.com>
2024-03-15 18:12:12 +0800

    Google 第三方登入

--- RGB.Back/RGB.Back/Controllers/MembersController.cs
+++ RGB.Back/RGB.Back/Controllers/MembersController.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.DependencyInjection;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using Google.Apis.Auth;
 
 
 
@@ -205,6 +206,95 @@ namespace RGB.Back.Controllers
 		{
 			HttpContext.Response.Cookies.Delete("Account");
 			//HttpContext.Response.Cookies.Delete("AccountUN");
+		}
+
+		/// <summary>
+		/// 驗證 Google 登入授權
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost("LoginGoogle")]
+		public async Task<System.String> ValidGoogleLogin()
+		{
+			string? formCredential = Request.Form["credential"]; //回傳憑證
+			string? formToken = Request.Form["g_csrf_token"]; //回傳令牌
+			string? cookiesToken = Request.Cookies["g_csrf_token"]; //Cookie 令牌
+			string result = "";
+
+			// 驗證 Google Token
+			GoogleJsonWebSignature.Payload? payload = await VerifyGoogleToken(formCredential, formToken, cookiesToken);
+			if (payload == null)
+			{
+				// 驗證失敗
+				result = "驗證 Google 授權失敗";
+			}
+			else
+			{
+				//驗證成功，取使用者資訊內容
+				result = "驗證 Google 授權成功" + "<br>";
+				result += "Email:" + payload.Email + "<br>";
+				result += "Name:" + payload.Name + "<br>";
+				result += "Picture:" + payload.Picture;
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// 驗證 Google Token
+		/// </summary>
+		/// <param name="formCredential"></param>
+		/// <param name="formToken"></param>
+		/// <param name="cookiesToken"></param>
+		/// <returns></returns>
+
+		private async Task<GoogleJsonWebSignature.Payload?> VerifyGoogleToken(string? formCredential, string? formToken, string? cookiesToken)
+		{
+			// 檢查空值
+			if (formCredential == null || formToken == null && cookiesToken == null)
+			{
+				return null;
+			}
+
+			GoogleJsonWebSignature.Payload? payload;
+			try
+			{
+				// 驗證 token
+				if (formToken != cookiesToken)
+				{
+					return null;
+				}
+
+				// 驗證憑證
+				IConfiguration Config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+				string GoogleApiClientId = Config.GetSection("GoogleApiClientId").Value;
+				var settings = new GoogleJsonWebSignature.ValidationSettings()
+				{
+					Audience = new List<string>() { GoogleApiClientId }
+				};
+				payload = await GoogleJsonWebSignature.ValidateAsync(formCredential, settings);
+				if (!payload.Issuer.Equals("accounts.google.com") && !payload.Issuer.Equals("https://accounts.google.com"))
+				{
+					return null;
+				}
+				if (payload.ExpirationTimeSeconds == null)
+				{
+					return null;
+				}
+				else
+				{
+					DateTime now = DateTime.Now.ToUniversalTime();
+					DateTime expiration = DateTimeOffset.FromUnixTimeSeconds((long)payload.ExpirationTimeSeconds).DateTime;
+					if (now > expiration)
+					{
+						return null;
+					}
+				}
+			}
+			catch
+			{
+				return null;
+			}
+			return payload;
 		}
 	}
 }
