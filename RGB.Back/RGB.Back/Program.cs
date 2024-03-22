c7be5c15c5f5de8b30c9856726540d321ad2f246
CrownChen <smilett07@gmail.com>
2024-03-22 15:32:08 +0800

    合併後0322版

--- RGB.Back/RGB.Back/Program.cs
+++ RGB.Back/RGB.Back/Program.cs
@@ -21,56 +21,19 @@ namespace RGB.Back
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
-			//jwt
 
-			builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+            builder.Services.AddDbContext<RizzContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Rizz"));
+            });
 
-
-			var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
-
-			TokenValidationParameters tokenValidationParams = new TokenValidationParameters
-			{
-				RequireExpirationTime = false,
-				ValidateIssuer = false,
-				ValidateAudience = false,
-
-				//驗證IssuerSigningKey
-				ValidateIssuerSigningKey = true,
-				//以JwtConfig:Secret為Key，做為Jwt加密
-				IssuerSigningKey = new SymmetricSecurityKey(key),
-
-				//驗證時效
-				ValidateLifetime = true,
-
-				//設定token的過期時間可以以秒來計算，當token的過期時間低於五分鐘時使用。
-				ClockSkew = TimeSpan.Zero
-			};
-
-			//註冊tokenValidationParams，後續可以注入使用。
-			builder.Services.AddSingleton(tokenValidationParams);
-
-			builder.Services.AddAuthentication(options =>
-			{
-				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			})
-			.AddJwtBearer(jwt =>
-			{
-				jwt.SaveToken = true;
-				jwt.TokenValidationParameters = tokenValidationParams;
-			});
-			//以上jwt
-			// Add services to the container.
-			builder.Services.AddDbContext<RizzContext>(options =>
-			{
-				options.UseSqlServer(builder.Configuration.GetConnectionString("Rizz"));
-			});
+           
+			
 
 			builder.Services.AddSignalR()
 							.AddJsonProtocol(options =>
 							{
-								options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+											  options.PayloadSerializerOptions.PropertyNamingPolicy = null;
 							});
 
             builder.Services.AddScoped<ChatService>();
@@ -116,9 +79,9 @@ namespace RGB.Back
 			{
 				options.Transports = HttpTransportType.WebSockets | HttpTransportType.LongPolling;
 			});
-            
-           
-			app.Run();
+            app.MapControllers();
+
+            app.Run();
 		}
         }
     }
